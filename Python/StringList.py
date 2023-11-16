4ceb9dcef89b2366216c83d397bddddd9c1c51b1
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-17 04:49:07 +0530

    4th

---
+++ Python/StringList.py
@@ -0,0 +1,7 @@
+# the generate_string_list function iterates through each number in the input list (numbers). Depending on the specified conditions:
+
+# If a number is divisible by both 2 and 4, it appends "I'm" to the result list.
+# If a number is not divisible by any number but 1 or itself (a prime number), it appends "Prime" to the result list.
+# If a number is divisible by 7, it appends "with" to the result list.
+# If a number is divisible by both 3 and 5, it appends "Coding" to the result list.
+# Otherwise, it appends the string representation of the number.
