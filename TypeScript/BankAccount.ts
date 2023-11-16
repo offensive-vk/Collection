d1f9bf52a5935a70790771a82771c9adade799b7
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-16 20:13:09 +0530

    4th

---
+++ TypeScript/BankAccount.ts
@@ -0,0 +1,15 @@
+/**
+
+You are tasked with implementing a simple banking system. Create a TypeScript class BankAccount with the following features:
+
+1. The class should have properties for accountNumber (string), accountHolder (string), and balance (number).
+
+2. Implement a constructor that initializes the accountNumber and accountHolder. The balance should be initialized to 0.
+
+3. Include methods deposit and withdraw to modify the balance.
+
+4. Implement a method getBalance that returns the current balance.
+
+Write a TypeScript program that creates an instance of BankAccount, performs some transactions, and prints the final balance.
+ */
+
--- TypeScript/Grades.ts
+++ TypeScript/Grades.ts
@@ -1,3 +1,6 @@
+/**
+ * You are given an array of objects representing students. Each student has a name (string) and an array of grades (numbers). Write a TypeScript function that takes an array of students and returns an object containing the average grade for each student. The result should be an object where the keys are student names, and the values are their average grades.
+ */
 interface Student {
     name: string;
     grades: number[];
