56b6879ef1a811f02afcf35994ae62658aff3b45
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-16 20:27:14 +0530

    5th

---
+++ TypeScript/MaxProduct.ts
@@ -0,0 +1,3 @@
+/**
+ * Write a TypeScript function that takes an array of numbers and finds the maximum product of any two distinct numbers in the array. If no such product exists, the function should return 0.
+ */
