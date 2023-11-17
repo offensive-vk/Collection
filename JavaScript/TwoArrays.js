f67d4597a17a0c80903f03c6874824bf9e273888
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-17 14:14:12 +0530

    6th

--- JavaScript/TwoArrays.js
+++ JavaScript/TwoArrays.js
@@ -1,3 +1,19 @@
 /**
  * In this problem, array1 and array2 are iterated simultaneously using a for loop. The loop runs for the length of either array (assuming both arrays have the same length), and the values from both arrays at the same index are outputted as a pair.
  */
+// Sample arrays
+const array1 = [147, 792, 355, 414];
+const array2 = ['first', 'second', 'third', 'fourth'];
+
+// Ensure both arrays have the same length
+if (array1.length !== array2.length) {
+    
+    console.error('Arrays must have the same length');
+} else {
+
+    // Iterate over the arrays simultaneously
+    for (let i = 0; i < array1.length; i++) {
+
+        console.log(`Pair ${i + 1}: ${array1[i]}, ${array2[i]}`);
+    }
+}
