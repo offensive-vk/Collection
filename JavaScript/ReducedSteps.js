824449a9d45538e73ee2ea0bda6b5545083edac8
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-17 20:24:03 +0530

    9th

---
+++ JavaScript/ReducedSteps.js
@@ -0,0 +1,3 @@
+/**
+ * A function that calculates the number of steps needed to reduce a given number to 0. The steps involve dividing the number by 2 if it's even or subtracting 1 if it's odd, and repeating until the number becomes 0:
+ */
