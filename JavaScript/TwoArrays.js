4ed9d7fe7fef188c8de641cc7bd195b1eeecf7ec
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-17 14:12:02 +0530

    6th

---
+++ JavaScript/TwoArrays.js
@@ -0,0 +1,3 @@
+/**
+ * In this problem, array1 and array2 are iterated simultaneously using a for loop. The loop runs for the length of either array (assuming both arrays have the same length), and the values from both arrays at the same index are outputted as a pair.
+ */
