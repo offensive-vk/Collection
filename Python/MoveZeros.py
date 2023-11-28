2b4071d3a0d63a88bf8461daad69b139ed3df326
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-28 14:50:46 +0530

    `0`s

---
+++ Python/MoveZeros.py
@@ -0,0 +1,2 @@
+# Create a function that takes a list of numbers as parameters and returns the list after moving all the zeros of the list to the end of the list , while maintaining the order of the other numbers.
+
