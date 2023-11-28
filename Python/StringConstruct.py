6fa96a534e766fc30ed767aef02ac2681ecacddf
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-28 15:17:41 +0530

    Str

---
+++ Python/StringConstruct.py
@@ -0,0 +1,22 @@
+# Check if the characters in the letters string can be used ti construct any of the word strings without repeating any letters. Returns true if it can be constructed and false otherwise.
+
+def canConstruct(letters, words):
+    for word in words:
+        availableLetters = set(letters)
+        for letter in word:
+            if letter not in availableLetters:
+                return False
+            else:
+                availableLetters.remove(letter)
+    return True
+
+letters = "ABCDEFGH"
+words = ["ABC", "DEF", "GHI"]  # True
+
+letters = "ABCD"
+words = ["ABCFG", "ABCDE", "EFGH"]  # False
+
+letters = "ASDFG"
+words = ["ASD", "FG", "GFD"]  # False
+
+print(canConstruct("abnnaan", "banana"))
