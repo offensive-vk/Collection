cc4faf58947db7134c9c72777d5914307ebbe0e3
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-29 17:54:14 +0530

    *

---
+++ TypeScript/TwoAsyncArrays.ts
@@ -0,0 +1,3 @@
+/**
+ * @abstract Write a function that takes two arrays of any data type and outputs each value of both arrays as a pair or simultaneously iterates over both arrays and returns an object as keys of first array with values of second array.
+ */
