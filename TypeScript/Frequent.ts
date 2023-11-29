5f252c399d81cc202b2f201c126be33769d55e3f
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-29 16:21:15 +0530

    *

---
+++ TypeScript/Frequent.ts
@@ -0,0 +1,3 @@
+/**
+ * Find the most frequent element in an array with a time complexity better than O(n log n), you can use a linear time algorithm. One way to achieve this is by using a hash map to count the occurrences of each element. 
+ */
