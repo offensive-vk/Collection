ca7cb48b02437534957ab806136b775c35afbda8
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-29 16:26:04 +0530

    *

--- TypeScript/Frequent.ts
+++ TypeScript/Frequent.ts
@@ -1,3 +1,17 @@
 /**
  * Find the most frequent element in an array with a time complexity better than O(n log n), you can use a linear time algorithm. One way to achieve this is by using a hash map to count the occurrences of each element. 
  */
+
+function mostFrequent(numbers: Array<number>): number {
+    if(numbers.length === 0) {
+        console.log(`Empty Array: ${numbers}\n`);
+        return 0;
+    }
+
+    // To count the occurrences of each element
+    const elementCounts = new Map<number, number>();
+
+    let maxCount = 0;
+    let mostFrequent = 0;
+
+}
