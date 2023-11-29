d5526acd25bc8bd9b6bce5e9f217bad69570166a
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-29 17:01:57 +0530

    *

---
+++ TypeScript/MoveZeros.ts
@@ -0,0 +1,4 @@
+/**
+ * Move all 0's to the end of the array without using sort(),  iterate through the array and reorder the elements accordingly. 
+ * @param {*} numbers number array to operate on.
+ */
