5b87a4c4758b8fbd18da23a156533ecd7ea77859
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-30 12:32:34 +0530

    *

--- TypeScript/Fibonacci.ts
+++ TypeScript/Fibonacci.ts
@@ -1,3 +1,29 @@
 /**
  * Write a function to generate the nth Fibonacci number.
  */
+
+function fibonacci(n: number): number {
+    if (n <= 0) {
+        throw new Error("Input must be a positive integer.");
+    }
+
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+
+    let prev = 1;
+    let current = 1;
+
+    for (let i = 3; i <= n; i++) {
+        const next = prev + current;
+        prev = current;
+        current = next;
+    }
+
+    return current;
+}
+
+// Example usage:
+const n = 7;
+const result = fibonacci(n);
+console.log(`The ${n}th Fibonacci number is: ${result}`);
