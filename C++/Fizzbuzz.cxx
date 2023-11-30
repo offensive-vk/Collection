d3d073408738a7b6d5ec727584e2c2714af528da
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-30 13:36:40 +0530

    *

---
+++ C++/Fizzbuzz.cxx
@@ -0,0 +1,4 @@
+/**
+ * FizzBuzz:
+Implement the FizzBuzz program. Print numbers from 1 to N, but for multiples of 3, print "Fizz," for multiples of 5, print "Buzz," and for multiples of both 3 and 5, print "FizzBuzz."
+*/
