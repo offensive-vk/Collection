8199ddb6d08a525bfd83fedd019a0797ac612cab
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-13 18:21:52 +0530

    **

---
+++ TypeScript/FilteredArray.ts
@@ -0,0 +1,9 @@
+/**
+ * TS/JS: Create an function that takes an array of any data type as input and does the following:
+    Checks how many elements of different data types are present in the given array
+    Returns different arrays for different elements of diff data types.
+    one for numbers
+    one for boolean numbers
+    one for string numbers
+    one for extra that can't be determined
+ */
