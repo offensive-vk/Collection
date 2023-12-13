ba5ae7045542d76b5a9492d06c5aeb19757f1387
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-13 18:36:42 +0530

    *

--- TypeScript/FilteredArray.ts
+++ TypeScript/FilteredArray.ts
@@ -7,3 +7,28 @@
     one for string numbers
     one for extra that can't be determined
  */
+
+function FilterArray<T> (arr: Array<T>): Set<Array<T>> {
+    
+    var numbers = []
+    var strings = []
+    var booleans = []
+    var others: any[] = []
+
+    //if(arr.length === 0) return new Set<T[]>(arr);
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == 'number') {
+            numbers.push(arr[i]);
+        } 
+        if (typeof arr[i] == 'string') {
+            strings.push(arr[i]);
+        } 
+        if (typeof arr[i] == 'boolean') {
+            booleans.push(arr[i]);
+        } 
+        else {
+            others.push(arr[i]);
+        }
+    }
+    return {} as Set<Array<T>>;
+}
