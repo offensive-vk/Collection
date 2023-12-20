b639efb9868bc238d8fd04c60589efc90ad42bce
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-20 18:45:27 +0530

    6th

--- C++/ComparatorExample.java
+++ C++/ComparatorExample.java
@@ -26,3 +26,24 @@ class Person {
         return age;
     }
 }
+public class ComparatorExample {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Alice", 30));
+        people.add(new Person("Bob", 25));
+        people.add(new Person("Charlie", 35));
+
+        // Using a Comparator to sort by age
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person p1, Person p2) {
+                return Integer.compare(p1.getAge(), p2.getAge());
+            }
+        });
+
+        // Display the sorted list
+        for (Person person : people) {
+            System.out.println(person.getName() + ", " + person.getAge() + " years old");
+        }
+    }
+}
