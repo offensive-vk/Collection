e636ec3c1c5a419ce394d777e86dc6a7884146ca
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-20 18:38:06 +0530

    Refractor `master` branch of Collection. (#15)
    * Create apisec-scan.yml
    
    * Added: `C++` Programs. (Few) (#9)
    
    * Create codeql.yml
    
    * Revert "Create apisec-scan.yml"
    
    This reverts commit d2cc64c87b7b2fb6aa1029b8be10aa270b118381.
    
    * Update codeql.yml
    
    * c++
    
    * Sorting
    
    * *
    
    * *
    
    * devskim.yml (#12)
    
    @offensive-vk Great job 🙌

---
+++ C++/BubbleSort.cxx
@@ -0,0 +1,43 @@
+/**
+ * Implement bubble sorting algorithm.
+ * take size of array and elements as input and sort the array accordingly.
+*/
+#include <iostream>
+#include <conio.h>
+
+int& BubbleSort(int arr[], int size) {
+    int x = size;
+    
+    for(int i = 0; i < x - 1; i++) {
+        for(int j = 0; j < x - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return *arr;
+}
+int main(int argc, char **argv[]) {
+    
+    int size = 0;
+    std::cout << "Please Enter The Size of Array : " << std::endl;
+    std::cin >> size;
+    int arr[size];
+
+    for(int i = 0; i < size; i++) {
+        std::cout << "Enter The Element At Index : " << i << std::endl;
+        std::cin >> arr[i];
+    }
+
+    BubbleSort(arr, size);
+    std::cout << "\n==================\n" << std::endl;
+    std::cout << "Array Elements After Sorting : \n" << std::endl;
+
+    for(int val : arr) {
+        std::cout << val << std::endl;
+    }
+
+    return 0;
+}
