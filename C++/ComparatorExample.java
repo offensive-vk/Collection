d3ba4b89885f37fe805cc501bed365eb23928b5e
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-20 18:45:08 +0530

    6th

--- C++/ComparatorExample.java
+++ C++/ComparatorExample.java
@@ -8,3 +8,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
