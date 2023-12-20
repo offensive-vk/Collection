6a7e5998ac0d92d646d2ffebbb91c3777280e9d1
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-20 18:44:45 +0530

    6th

---
+++ C++/ComparatorExample.java
@@ -0,0 +1,10 @@
+/**
+ * In Java, the Comparator interface is similar to Comparable in that it is used for comparing objects, but it provides a more flexible way to define custom ordering for classes that may not have a natural order or when you want to define multiple ways to order objects.
+
+ * The compare method compares its two arguments for order, and it returns a negative integer, zero, or a positive integer as the first argument is less than, equal to, or greater than the second.
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
