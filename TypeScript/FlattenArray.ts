842721bda9115a5bfed77178b4e230f35b54b310
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-02 19:04:25 +0530

    12th

---
+++ TypeScript/FlattenArray.ts
@@ -0,0 +1,5 @@
+/**
+ * @description This TypeScript program defines a function flattenArray that takes an array as input and returns a new array with all nested arrays flattened. The example usage demonstrates how to flatten a nested array.
+ * @param arr Array to flatten
+ * @returns a new array
+ */
