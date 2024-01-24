f5657e9313a09b7a92d12e1382ba558fd2424bcd
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-24 17:18:39 +0530

    Verified: *.java

--- Java/StaticBlock.java
+++ Java/StaticBlock.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  * Program to show the usage of static initialization block and static keyword.
  */
 public class StaticBlock {
-	static Scanner input = new Scanner(System.in);
+	private static Scanner input = new Scanner(System.in);
 
     public static boolean flag = false;
     public static int B = input.nextInt();
@@ -18,8 +18,8 @@ public class StaticBlock {
             System.out.println("java.lang.Exception: Breadth and height must be positive");
     }
 
-	public static void main(String[] args){
-		if(flag){
+	public static void main(String[] args) {
+		if(flag) {
 			int area = B * H;
 			System.out.print(area);
 		}	
